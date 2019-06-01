9bfadc40f424c274fef3febf531c6faaf9371feb
joe munoz <joe.rob.munoz@gmail.com>
2019-06-01 01:15:39 -0400

    Messing around with CodeMirror keymap override.

--- main.js
+++ main.js
@@ -3,11 +3,50 @@ define([
     'base/js/events'
     ], function(Jupyter, events) {
 
-    function load_ipython_extension() {
-        const KEY_BINDING = 'CTRL, D'
-        Jupyter.keyboard_manager.command_shortcuts.remove_shortcut(KEY_BINDING);
-        Jupyter.keyboard_manager.command_shortcuts.add_shortcut(KEY_BINDING, 'jupyter-notebook:restart-kernel');
-    }
+    var initialize = function () {
+        // update params with any specified in the server's config file.
+        // the "thisextension" value of the Jupyter notebook config's
+        // data may be undefined, but that's ok when using JQuery's extend
+        $.extend(true, params, Jupyter.notebook.config.thisextension);
+
+        // add our extension's css to the page
+        $('<script/>')
+            .attr({
+                // rel: 'script',
+                type: 'text/js',
+                href: requirejs.toUrl('./vendor/searchCursor.js')
+            })
+            .appendTo('head');
+    };
+
+    var onCommand = function(event) {
+        console.log("Bulk edit!");
+    };
+    
+    var load_ipython_extension = function () {
+        // Once the config has been loaded, do everything else.
+        // The loaded object is a javascript Promise object
+        const KEY_BINDING = 'CTRL-D';
+        
+        // Ignore CodeMirror keymap defaults during edit mode
+        // CodeMirror.keyMap.default[KEY_BINDING] = null;
+        delete CodeMirror.keyMap.default[KEY_BINDING];
+        
+        if (!Jupyter.keyboard_manager.edit_shortcuts.is_available_shortcut(KEY_BINDING)) {
+            Jupyter.keyboard_manager.edit_shortcuts.remove_shortcut(KEY_BINDING);
+        }
+
+        Jupyter.keyboard_manager.edit_shortcuts.add_shortcut(KEY_BINDING, {
+            help : 'bulk edit',
+            help_index : 'zz',
+            handler : function (event) {
+                onCommand(event);
+                return false;
+            }}
+        );
+
+        return Jupyter.notebook.config.loaded.then(initialize);
+    };
 
     // selectNextOccurrence
     // var cell = Jupyter.notebook.get_selected_cell();
