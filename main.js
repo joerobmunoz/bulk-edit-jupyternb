c281271944a87db5149bee715673e73c2cf0ba35
joe munoz <joe.rob.munoz@gmail.com>
2019-05-31 22:53:06 -0400

    Added searchCursor.js vendor lib to support codemirror multi cursor movements.

--- main.js
+++ main.js
@@ -8,6 +8,11 @@ define([
         Jupyter.keyboard_manager.command_shortcuts.remove_shortcut(KEY_BINDING);
         Jupyter.keyboard_manager.command_shortcuts.add_shortcut(KEY_BINDING, 'jupyter-notebook:restart-kernel');
     }
+
+    // selectNextOccurrence
+    // var cell = Jupyter.notebook.get_selected_cell();
+    // cell.code_mirror.listSelections()
+
         
     return {
         load_ipython_extension: load_ipython_extension
