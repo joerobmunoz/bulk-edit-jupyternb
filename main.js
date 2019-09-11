0190403663e1cd55f909de7e53e7a1ac03fb7bd3
joe munoz <joe.rob.munoz@gmail.com>
2019-09-10 23:37:12 -0400

    hotkey override.

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+// define([
+//     'base/js/namespace'
+// ], function(
+//     Jupyter
+// ) {
+//     function load_ipython_extension() {
+//         console.log("LOADED!!!");
+//         console.log(
+//             'This is the current notebook application instance:',
+//             Jupyter.notebook
+//         );
+//     }
+
+//     return {
+//         load_ipython_extension: load_ipython_extension
+//     };
+// });
+
+
 define([
     'base/js/namespace',
     'base/js/events'
@@ -21,29 +40,53 @@ define([
 
     var onCommand = function(event) {
         console.log("Bulk edit!");
+        let cm = event.notebook.get_selected_cell().code_mirror;
+        let selections = cm.listSelections();
+
+        if (selections.length > 0) {
+            // has current selection
+            let first_anch = selections[0]['anchor'];
+            let last_head = selections[selections.length-1]['head'];
+            
+            let leftover = 
+            // grab all text after head
+
+            return false;
+        }
+        
+        if (selections.every( (val, i, arr) => val === arr[0] )) {
+            // Invalid bulk edit selections, none selected
+            return false;
+        }
+
+        // else, select current word
+
+        return false;
     };
     
     var load_ipython_extension = function () {
+
         // Once the config has been loaded, do everything else.
         // The loaded object is a javascript Promise object
-        const KEY_BINDING = 'CTRL-D';
+        const PC_KEY_BINDING = 'Ctrl-D';
+        const MAC_KEY_BINDING = 'Cmd-D'
+        const bindings = [PC_KEY_BINDING, MAC_KEY_BINDING]
         
         // Ignore CodeMirror keymap defaults during edit mode
-        // CodeMirror.keyMap.default[KEY_BINDING] = null;
-        delete CodeMirror.keyMap.default[KEY_BINDING];
+        delete CodeMirror.keyMap.default[PC_KEY_BINDING];
+        delete CodeMirror.keyMap.pcDefault[PC_KEY_BINDING];
+        delete CodeMirror.keyMap.macDefault[MAC_KEY_BINDING];
         
-        if (!Jupyter.keyboard_manager.edit_shortcuts.is_available_shortcut(KEY_BINDING)) {
-            Jupyter.keyboard_manager.edit_shortcuts.remove_shortcut(KEY_BINDING);
-        }
-
-        Jupyter.keyboard_manager.edit_shortcuts.add_shortcut(KEY_BINDING, {
-            help : 'bulk edit',
-            help_index : 'zz',
-            handler : function (event) {
-                onCommand(event);
-                return false;
-            }}
-        );
+        bindings.map((key) => {
+            Jupyter.keyboard_manager.edit_shortcuts.add_shortcut(key, {
+                help : 'bulk edit',
+                help_index : 'zz',
+                handler : function (event) {
+                    onCommand(event);
+                    return false;
+                }}
+            );
+        });
 
         return Jupyter.notebook.config.loaded.then(initialize);
     };
